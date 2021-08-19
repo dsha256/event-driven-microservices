91f36fd025c7bf9d81bff9a2ffbb9f40c8a435fc
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-19 22:40:01 +0400

    Created The Command Bus

--- ProductService/src/main/java/com/davidcorp/estore/ProductService/rest/ProductsController.java
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/rest/ProductsController.java
@@ -1,19 +1,44 @@
 package com.davidcorp.estore.ProductService.rest;
 
+import com.davidcorp.estore.ProductService.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
     @Autowired
-    private Environment env;
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
 
     @PostMapping
     public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
-        return "HTTP POST Handled " + createProductRestModel.getTitle();
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString()).build();
+
+        String returnValue;
+
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
     }
 
     @GetMapping
