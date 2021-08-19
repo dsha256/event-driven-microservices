f426d898c58b044817ff5a88989b745219af2c18
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-19 22:55:45 +0400

    Added the vallidation for CreateProductCommand

--- ProductService/src/main/java/com/davidcorp/estore/ProductService/command/ProductAggregate.java
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/command/ProductAggregate.java
@@ -3,6 +3,8 @@ package com.davidcorp.estore.ProductService.command;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.math.BigDecimal;
+
 @Aggregate
 public class ProductAggregate {
 
@@ -13,6 +15,14 @@ public class ProductAggregate {
     @CommandHandler
     public ProductAggregate(CreateProductCommand createProductCommand) {
         // Validate Create Product Command
+
+        if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Price cannot be less or equal than zero");
+        }
+
+        if (createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
     }
 
 }
