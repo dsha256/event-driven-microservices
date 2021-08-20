1dc9f67b8056699546f69c678d2b13a1139f5cff
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-20 21:39:38 +0400

    Created a new Command interceptor class

---
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/command/interceptors/CreateProductCommandInterceptor.java
@@ -0,0 +1,43 @@
+package com.davidcorp.estore.ProductService.command.interceptors;
+
+import com.davidcorp.estore.ProductService.command.CreateProductCommand;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.messaging.MessageDispatchInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.BiFunction;
+
+@Component
+public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateProductCommandInterceptor.class);
+
+    @Override
+    public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(List<? extends CommandMessage<?>> list) {
+
+        return (index, command) -> {
+
+            LOGGER.info("Intercepted command: " + command.getPayloadType());
+
+            if (CreateProductCommand.class.equals(command.getPayloadType())) {
+
+                CreateProductCommand createProductCommand = (CreateProductCommand) command.getPayload();
+
+                if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+                    throw new IllegalArgumentException("Price cannot be less or equal than zero");
+                }
+                if (createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+                    throw new IllegalArgumentException("Title cannot be empty");
+                }
+
+            }
+
+            return command;
+        };
+
+    }
+}
