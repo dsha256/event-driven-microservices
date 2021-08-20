2c3e6dae3aa9c6283aecf68d64c6240f8b812ee4
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-20 21:20:57 +0400

    Impelemnted Bean Validation

--- ProductService/src/main/java/com/davidcorp/estore/ProductService/command/rest/ProductsCommandController.java
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/command/rest/ProductsCommandController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -23,7 +24,7 @@ public class ProductsCommandController {
     }
 
     @PostMapping
-    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+    public String createProduct(@Valid @RequestBody CreateProductRestModel createProductRestModel) {
 
         CreateProductCommand createProductCommand = CreateProductCommand.builder()
                 .price(createProductRestModel.getPrice())
--- ProductService/src/main/java/com/davidcorp/estore/ProductService/rest/CreateProductRestModel.java
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/rest/CreateProductRestModel.java
@@ -2,13 +2,22 @@ package com.davidcorp.estore.ProductService.rest;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class CreateProductRestModel {
 
+    @NotBlank(message = "Product title is a required field")
     private String title;
+
+    @Min(value = 1, message = "Price can not be lower than 1")
     private BigDecimal price;
+
+    @Min(value = 1, message = "Quantity can not be lower than 1")
+    @Max(value = 5, message = "Price can not be larger than 5")
     private Integer quantity;
 
 }
