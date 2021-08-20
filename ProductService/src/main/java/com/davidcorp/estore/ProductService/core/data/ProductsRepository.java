33f7fd9a5641124d07c4cbae2d00b038f0030181
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-20 14:20:08 +0400

    Created Product Repository

---
+++ ProductService/src/main/java/com/davidcorp/estore/ProductService/core/data/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.davidcorp.estore.ProductService.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<ProductEntity, String> {
+
+    ProductEntity findByProductId(String productId);
+    ProductEntity findByProductIdOrTitle(String productId, String title);
+}
