c8960e8a43fe0b8e9cb169902ad031d5fb3e16ec
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-21 21:05:22 +0400

    Created OrderService

---
+++ OrderService/src/test/java/com/davidcorp/estore/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcorp.estore.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
