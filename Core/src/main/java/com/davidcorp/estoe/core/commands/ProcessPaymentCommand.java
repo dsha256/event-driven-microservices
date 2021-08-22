c68f58ac25f820d650a149fc57eb7705b9257b97
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-22 21:11:55 +0400

    Cretaed ProcessPaymentCommand

---
+++ Core/src/main/java/com/davidcorp/estoe/core/commands/ProcessPaymentCommand.java
@@ -0,0 +1,16 @@
+package com.davidcorp.estoe.core.commands;
+
+import com.davidcorp.estoe.core.model.PaymentDetails;
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Builder
+@Data
+public class ProcessPaymentCommand {
+
+    @TargetAggregateIdentifier
+    private final String paymentId;
+    private final String orderId;
+    private final PaymentDetails paymentDetails;
+}
