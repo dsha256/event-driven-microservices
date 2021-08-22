83e24587c73cef2f76e15bf777ba7b4d734d1841
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-22 17:40:55 +0400

    Created the UsersService module

---
+++ UsersService/src/main/java/com/davidcorp/estore/usersservice/UsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.davidcorp.estore.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+}
---
+++ UsersService/src/test/java/com/davidcorp/estore/usersservice/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcorp.estore.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
