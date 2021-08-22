37cbdc21f791c48dd897ada03e03c5fced5d4b08
David SHainidze <david.shainidze.1@btu.edu.ge>
2021-08-22 21:34:01 +0400

    Created the PaymentsService module

---
+++ PaymentsService/src/test/java/com/davidcorp/paymentsservice/PaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcorp.paymentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
